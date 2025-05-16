0544b04e7d64bc49928d3f0979c354b14c7a6ae6
edu-hc <edu.carvc@gmail.com>
2025-05-16 18:30:49 -0300

    init: project creation

---
+++ src/test/java/com/edu/encurtador/EncurtadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.encurtador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
