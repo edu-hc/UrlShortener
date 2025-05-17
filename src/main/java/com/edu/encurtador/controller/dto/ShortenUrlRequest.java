ff388ff686ec7e0fda6048c480fa792e2df5aff7
edu-hc <edu.carvc@gmail.com>
2025-05-17 18:04:25 -0300

    feat: DTOs

---
+++ src/main/java/com/edu/encurtador/controller/dto/ShortenUrlRequest.java
@@ -0,0 +1,4 @@
+package com.edu.encurtador.controller.dto;
+
+public record ShortenUrlRequest(String url) {
+}
---
+++ src/main/java/com/edu/encurtador/controller/dto/ShortenUrlResponse.java
@@ -0,0 +1,4 @@
+package com.edu.encurtador.controller.dto;
+
+public record ShortenUrlResponse(String shortenedUrl) {
+}
